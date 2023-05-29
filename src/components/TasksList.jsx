a872d08a13d13804dbc782759ef7b5feff9865f5
leonelborgesdev <leonel03t@gmail.com>
2023-05-29 11:09:52 -0400

    taskslist

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,12 +1,23 @@
 import { useGetTasksQuery } from "../api/apiSlice";
 
 function TasksList() {
-  const { data, isError, isLoading, error } = useGetTasksQuery();
+  const { data: tasks, isError, isLoading, error } = useGetTasksQuery();
   if (isLoading) return <div>Loading...</div>;
   else if (isError) return <div>Error: {error.message}</div>;
 
-  console.log(data);
-  return <div>TasksList</div>;
+  return (
+    <ul>
+      {tasks.map((task) => (
+        <li key={task.id}>
+          <h3>{task.name}</h3>
+          <p>{task.description}</p>
+          <button>delete</button>
+          <input type="checkbox" name="" id={task.id} />
+          <label htmlFor={task.id}>Completed</label>
+        </li>
+      ))}
+    </ul>
+  );
 }
 
 export default TasksList;
