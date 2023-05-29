2ea24a32cf0153c9478ae137f6acb88172688a8a
leonelborgesdev <leonel03t@gmail.com>
2023-05-29 10:51:46 -0400

    reac-redux

--- src/components/TasksList.jsx
+++ src/components/TasksList.jsx
@@ -1,4 +1,11 @@
+import { useGetTasksQuery } from "../api/apiSlice";
+
 function TasksList() {
+  const { data, isError, isLoading, error } = useGetTasksQuery();
+  if (isLoading) return <div>Loading...</div>;
+  else if (isError) return <div>Error: {error.message}</div>;
+
+  console.log(data);
   return <div>TasksList</div>;
 }
 
